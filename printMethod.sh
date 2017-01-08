bd3a103d2a4e2e967d70acb1ba3e6826143d221e
sloth5241 <zhaocong5241@126.com>
2017-01-08 16:05:01 +0800

    增加输出彩色字体

--- printMethod.sh
+++ printMethod.sh
@@ -6,3 +6,17 @@ echo 'hello world !'
 echo "hello world !"
 #printf
 printf "hello world ! \n"
+printf "%1.2f \n" 443.4123
+
+#echo 使用转义字符
+echo -e "1\t2\t3"  #使用注意字符-e
+
+#彩色打印
+echo -e "\033[30m 黑色字 \033[0m"
+echo -e "\033[31m 红色字 \033[0m"
+echo -e "\033[32m 绿色字 \033[0m"
+echo -e "\033[33m 黄色字 \033[0m"
+echo -e "\033[34m 蓝色字 \033[0m"
+echo -e "\033[35m 紫色字 \033[0m"
+echo -e "\033[36m 天蓝字 \033[0m"
+echo -e "\033[37m 白色字 \033[0m"
